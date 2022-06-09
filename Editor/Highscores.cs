3338e61fe035e05c64e1a0f1a1f29a43f7419250
Dennis Hawran <dennishawran@gmail.com>
2022-06-09 14:26:07 +0200

    1.0.1

--- Editor/Highscores.cs
+++ Editor/Highscores.cs
@@ -4,74 +4,77 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System;
 
-public class SingleNameScore
+namespace HighscorePlugin
 {
-    public string name { get; set; }
-    public int score { get; set; }
-}
-
-public class Highscores : MonoBehaviour
-{
-    public Action<List<SingleNameScore>> OnHighscoresReceived;
-
-    [SerializeField] private string gameName;
-    [SerializeField] private string highscoresUrl;
-
-    private List<SingleNameScore> highscores = new List<SingleNameScore>();
-
-    public void GetHighscores(int amount)
+    public class SingleNameScore
     {
-        StartCoroutine(LoadHighscores(amount));
+        public string name { get; set; }
+        public int score { get; set; }
     }
 
-    public void CreateHighscore(string name, int score)
+    public class Highscores : MonoBehaviour
     {
-        StartCoroutine(NewHighscore(name, score));
-    }
+        public Action<List<SingleNameScore>> OnHighscoresReceived;
 
-    private IEnumerator NewHighscore(string name, int score)
-    {
-        print("Creating highscore");
+        [SerializeField] private string gameName;
+        [SerializeField] private string highscoresUrl;
 
-        string url = highscoresUrl + "?game=" + gameName + "&command=create&name=" + name + "&score=" + score;
-        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        private List<SingleNameScore> highscores = new List<SingleNameScore>();
 
-        yield return uwr.SendWebRequest();
-
-        if (uwr.isNetworkError)
-            Debug.Log("Error While Sending: " + uwr.error);
-        else
-            Debug.Log("Received: " + uwr.downloadHandler.text);
-    }
-
-    private IEnumerator LoadHighscores(int amount)
-    {
-        print("Loading highscores");
-
-        string url = highscoresUrl + "?game=" + gameName + "&command=get&amount=" + amount;
-        UnityWebRequest uwr = UnityWebRequest.Get(url);
-        
-        yield return uwr.SendWebRequest();
-
-        if (uwr.isNetworkError)
-            Debug.Log("Error While Sending: " + uwr.error);
-        else
+        public void GetHighscores(int amount)
         {
-            string result = uwr.downloadHandler.text;
-            string[] results = result.Split('&');
-            List<SingleNameScore> newHighscores = new List<SingleNameScore>();
+            StartCoroutine(LoadHighscores(amount));
+        }
 
-            for (int i = 0; i < results.Length - 1; i = i + 2)
+        public void CreateHighscore(string name, int score)
+        {
+            StartCoroutine(NewHighscore(name, score));
+        }
+
+        private IEnumerator NewHighscore(string name, int score)
+        {
+            print("Creating highscore");
+
+            string url = highscoresUrl + "?game=" + gameName + "&command=create&name=" + name + "&score=" + score;
+            UnityWebRequest uwr = UnityWebRequest.Get(url);
+
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError)
+                Debug.Log("Error While Sending: " + uwr.error);
+            else
+                Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+
+        private IEnumerator LoadHighscores(int amount)
+        {
+            print("Loading highscores");
+
+            string url = highscoresUrl + "?game=" + gameName + "&command=get&amount=" + amount;
+            UnityWebRequest uwr = UnityWebRequest.Get(url);
+
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError)
+                Debug.Log("Error While Sending: " + uwr.error);
+            else
             {
-                SingleNameScore newHighscore = new SingleNameScore();
-                newHighscore.name = results[i];
-                newHighscore.score = int.Parse(results[i + 1]);
+                string result = uwr.downloadHandler.text;
+                string[] results = result.Split('&');
+                List<SingleNameScore> newHighscores = new List<SingleNameScore>();
 
-                newHighscores.Add(newHighscore);
+                for (int i = 0; i < results.Length - 1; i = i + 2)
+                {
+                    SingleNameScore newHighscore = new SingleNameScore();
+                    newHighscore.name = results[i];
+                    newHighscore.score = int.Parse(results[i + 1]);
+
+                    newHighscores.Add(newHighscore);
+                }
+
+                highscores = newHighscores;
+                OnHighscoresReceived?.Invoke(highscores);
             }
-
-            highscores = newHighscores;
-            OnHighscoresReceived?.Invoke(highscores);
         }
     }
 }
